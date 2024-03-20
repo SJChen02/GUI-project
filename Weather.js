92fc240cc79c2c00434163576c0089f83973fd36
ec22657 <163860375+ec22657@users.noreply.github.com>
2024-03-20 15:47:32 +0000

    Add files via upload
    done the weather details, I changed the UV section to sunrise

--- Weather.js
+++ Weather.js
@@ -6,11 +6,19 @@ async function getInfo(nlat,nlon){
     const info = json.list
     let timeResult = "";
     let tempResult = "";
-    for(let weatherDate=0; weatherDate<4; weatherDate++){
+    for(let weatherDate=0; weatherDate<5; weatherDate++){
         let next_time = new Date(info[weatherDate].dt_txt).toLocaleString(undefined,{hour: '2-digit', minute: '2-digit'});
-        timeResult+=  `<div style="padding-right: 1.5em; padding-left: 1.3em">${next_time}</div>`
-        tempResult+= `<div style="padding-right: 2em; padding-left: 1.3em"> ${Math.round(kToC(info[weatherDate].main.temp)) + "°"}</div>`
+        if (weatherDate==0){
+            timeResult+=  `<div class="Time_Item">Now</div>`
+        }
+        else{
+            timeResult+=  `<div class="Time_Item">${next_time}</div>`
+        }
+        
+        tempResult+= `<div class="Temperature-WeatherForecast-container"> ${Math.round(kToC(info[weatherDate].main.temp)) + "°"}</div>`
     }
+
+    
     document.getElementById('3hourTime').innerHTML= timeResult;
     document.getElementById('hourWeatherData').innerHTML= tempResult;
     document.getElementById('weatherHourIcon1').src = `${checkTypeIconOfWeather(info[0])}`;
@@ -24,10 +32,14 @@ async function getInfo(nlat,nlon){
     let tempDiffResult ="";
     let tempMax ="";
     let tempMin ="";
-    for(let day=8; day<info.length; day+=8){
+    for(let day=5; day<info.length; day+=8){
+        // let today = new Date().getDay();
         let week = new Date(info[day].dt_txt).getDay();
-        weekdayResult+= `<div style="padding-right: 1.5em; padding-left: 1.5em"> ${weekday[week]}</div>`
-        tempDiffResult+= `<div style="padding-right: 1em;padding-left: 1em;"> ${Math.ceil(kToC(info[day].main.temp_max)) + "°"}/${Math.floor(kToC(info[day].main.temp_min)) + "°"} </div>`
+
+
+        weekdayResult+= `<div class="Weekly_Time_container"> ${weekday[week]}</div>`
+        
+        tempDiffResult+= `<div class="Weekly_Time_container"> ${Math.ceil(kToC(info[day].main.temp_max)) + "°"}/${Math.floor(kToC(info[day].main.temp_min)) + "°"} </div>`
     }
     
     document.getElementById('weekDay').innerHTML= weekdayResult;
@@ -39,7 +51,6 @@ async function getInfo(nlat,nlon){
     document.getElementById('weatherWeekIcon5').src = `${checkTypeIconOfWeather(info[8*4])}`;
 
     document.getElementById('umbrellaTmrCheck').innerHTML= `${checkBringUmbrella(info[8])}`
-    
 }
 
 getInfo(51.5073, -0.1276);
@@ -51,8 +62,8 @@ async function getTodayInfo(nlat,nlon){
 
     const info = json.main
     document.getElementById('todayDateTemp').innerHTML= `${Math.round(kToC(info.temp)) + "°"}`;
-    document.getElementById('nowTemp').innerHTML= `<div style="padding-right: 2em"> ${Math.round(kToC(info.temp)) + "°"}</div>`;
-    document.getElementById('tdyDiffTemp').innerHTML = `<div style="padding-right: 1em;padding-left: 1.25em;"> ${Math.ceil(kToC(info.temp_max)) + "°"}/${Math.floor(kToC(info.temp_min)) + "°"} </div>`;
+    // document.getElementById('nowTemp').innerHTML= `<div class="Temperature-WeatherForecast> ${Math.round(kToC(info.temp)) + "°"}</div>`;
+    document.getElementById('differentTemp').innerHTML = `<div class="Weekly_Time_container"> ${Math.ceil(kToC(info.temp_max)) + "°"}/${Math.floor(kToC(info.temp_min)) + "°"} </div>`;
 
     const date = new Date()
     let day = date.getDate();
@@ -64,12 +75,41 @@ async function getTodayInfo(nlat,nlon){
 
     document.getElementById('umbrellaTdyCheck').innerHTML= `${checkBringUmbrella(json)}`;
     document.getElementById('weatherLocation').innerHTML= `${weatherLocation(json)}`;
+
+    document.getElementById('feelLikeData').innerHTML= `${Math.ceil(kToC(info.feels_like)) + "°"}`;
+    document.getElementById('humidityData').innerHTML= `${info.humidity + "%"}`;
+    document.getElementById('windDirectionData').innerHTML= `${degreesToCompass(json.wind.deg)}`;
+    document.getElementById('windSpeedData').innerHTML= `${Math.floor(msToMph(json.wind.speed)) + "mph"}`;
+    document.getElementById('pressureData').innerHTML= `${info.pressure + "hPa"}`;
+    document.getElementById('visibilityData').innerHTML= `${mToKm(json.visibility) + "km"}`;
+    document.getElementById('sunriseData').innerHTML= `${unixToTime(json.sys.sunrise)}`;
+    document.getElementById('sunsetData').innerHTML= `${unixToTime(json.sys.sunset)}`;
 }
 
 getTodayInfo(51.5073, -0.1276);
 
 function kToC(k){
     return k-273.15
+}
+
+function unixToTime(num){
+    let time = new Date(num*1000).toLocaleString(undefined, {hour: '2-digit', minute: '2-digit'})
+    return time;
+}
+
+function degreesToCompass(deg){
+    val=Math.floor((deg/22.5)+.5)
+    arr=["N","NNE","NE","ENE","E","ESE","SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    com=arr[(val % 16)]
+    return com;
+}
+
+function msToMph(ms){
+    return ms /0.44704
+}
+
+function mToKm(m){
+    return m /1000
 }
 
 function checkTypeNameOfWeather(tdy){
@@ -116,7 +156,6 @@ function checkBringUmbrella(data){
 async function getInfo2(sta){
     const res = await fetch('https://api.tfl.gov.uk/line/mode/tube/status')
     const json = await res.json()
-    console.log(json)
 
     document.getElementById('tubeStatus').innerHTML= json[sta].lineStatuses[0].statusSeverityDescription //first 0 depend on the line name
     document.getElementById('trianName').innerHTML= `${trianLineName(json[sta])}`;
@@ -128,7 +167,6 @@ getInfo2(7);
 async function getInfo3(loc){
     const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=b3e79ab765ac55b063567e2787d48609`)
     const json = await res.json()
-    console.log(json)
 
     getInfo(json[0].lat, json[0].lon);
     getTodayInfo(json[0].lat, json[0].lon);
